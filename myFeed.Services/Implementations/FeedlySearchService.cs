30b27bdd1cbac33bd1e668b927ca6867412a7135
Artjom Graf <prizrak.nra@gmail.com>
2017-09-14 15:55:17 +0300

    Make HttpClient static

--- myFeed.Services/Implementations/FeedlySearchService.cs
+++ myFeed.Services/Implementations/FeedlySearchService.cs
@@ -10,13 +10,14 @@ namespace myFeed.Services.Implementations
 {
     public class FeedlySearchService : ISearchService
     {
+        private static readonly HttpClient Client = new HttpClient();
+        
         public async Task<SearchRootEntity> Search(string query)
         {
             try
             {
                 var queryUrl = $@"http://cloud.feedly.com/v3/search/feeds?count=40&query=:{query}";
-                using (var client = new HttpClient())
-                using (var response = await client.GetAsync(queryUrl).ConfigureAwait(false))
+                using (var response = await Client.GetAsync(queryUrl).ConfigureAwait(false))
                 {
                     var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                     return JsonConvert.DeserializeObject<SearchRootEntity>(responseString);
