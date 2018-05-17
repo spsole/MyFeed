9d6f36208e82d28e0e25fe39228be1483c0d5894
Artjom Graf <prizrak.nra@gmail.com>
2018-05-17 12:00:08 +0300

    Prepare 4.0.0.0 release. Acrylic, MX, Fall Creators Update

---
+++ myFeed.Uwp/Converters/LocaleExtension.cs
@@ -0,0 +1,19 @@
+﻿using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace myFeed.Uwp.Converters
+{
+    [Bindable]
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public sealed class Locale : MarkupExtension
+    {
+        private static readonly ResourceLoader ResourceLoader = ResourceLoader.GetForViewIndependentUse();
+
+        protected override object ProvideValue() => ResourceLoader.GetString(Key);
+
+        public string Key { get; set; }
+
+        public Locale() { }
+    }
+}
